af6d895da0228f7f79fdaac7b3ddc6ec0da40642
ShaikeMukul <smuku004@ucr.edu>
2023-08-02 16:19:57 -0700

    Add listing initial design

---
+++ src/Components/AddListingModal/MainApp.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import { Button, Container, Row, Col, Card } from 'react-bootstrap';
+import AddListingModal from './AddListingModal';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { MdLocationOn } from 'react-icons/md';
+
+const MainApp = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [listings, setListings] = useState([]);
+
+  const handleAddListing = (newListing) => {
+    setListings([...listings, newListing]);
+  };
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <Container>
+      <Row className="mt-3">
+        <Col>
+          <Button onClick={handleShowModal} variant="primary">
+            Add Listing
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        {/* Map through the "listings" array and display each item in a ListingCard */}
+        {listings.map((item, index) => (
+          <Col key={index} xs={12} sm={6} md={4} lg={3}>
+            <ListingCard item={item} />
+          </Col>
+        ))}
+      </Row>
+      <AddListingModal
+        show={showModal}
+        handleClose={handleCloseModal}
+        handleAddListing={handleAddListing}
+      />
+      <style>
+        {`
+          /* Custom style for the listing cards */
+          .listing-card {
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            padding: 12px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            transition: box-shadow 0.3s;
+          }
+
+          .listing-card:hover {
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          }
+
+          .listing-card .card-img-top {
+            height: 200px;
+            object-fit: cover;
+            border-radius: 5px;
+          }
+
+          .listing-card .card-title {
+            font-size: 18px;
+            font-weight: bold;
+          }
+
+          .listing-card .card-text {
+            font-size: 16px;
+            color: #333;
+          }
+
+          .listing-card .favorite-button {
+            display: flex;
+            align-items: center;
+            padding: 8px;
+            border-radius: 5px;
+            background-color: #007bff;
+            color: #fff;
+            font-weight: bold;
+            cursor: pointer;
+            transition: background-color 0.2s;
+          }
+
+          .listing-card .favorite-button:hover {
+            background-color: #0056b3;
+          }
+
+          .listing-card .favorite-icon {
+            margin-right: 8px;
+          }
+        `}
+      </style>
+    </Container>
+  );
+};
+
+function FavoriteButton({ isFavorite, handleFavoriteClick }) {
+  return (
+    <div
+      className="favorite-button"
+      onClick={handleFavoriteClick}
+    >
+      {isFavorite ? (
+        <>
+          <AiFillHeart className="favorite-icon" />
+        </>
+      ) : (
+        <>
+          <AiOutlineHeart className="favorite-icon" />
+        </>
+      )}
+    </div>
+  );
+}
+
+const ListingCard = ({ item }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleFavoriteClick = () => {
+    setIsFavorite((prevIsFavorite) => !prevIsFavorite);
+  };
+
+  return (
+    <Card className="listing-card">
+      {/* Display item details */}
+      <Card.Img variant="top" src={URL.createObjectURL(item.pictures)} alt={item.itemName} />
+      <Card.Body>
+        <Card.Title>{item.itemName}</Card.Title>
+        <Card.Text>Price: ${item.price}</Card.Text>
+        {/* Create a new row for the location icon and favorite button */}
+        <Row className="align-items-center">
+          <Col xs={6}>
+            {/*location icon */}
+            <MdLocationOn style={{ marginRight: '8px' }} />
+            {item.location}
+          </Col>
+          <Col xs={6}>
+            <FavoriteButton
+              isFavorite={isFavorite}
+              handleFavoriteClick={handleFavoriteClick}
+            />
+          </Col>
+        </Row>
+        <Card.Text>Description: {item.description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default MainApp;
