ab54b204c529a9b189e704ed8dfb22b2d9b08b00
aarav885 <aaravpatel715@gmail.com>
2023-08-02 15:14:42 -0700

    added cors

--- mongo/schema.js
+++ mongo/schema.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
