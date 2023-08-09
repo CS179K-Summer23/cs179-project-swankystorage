afa5462a3362dbacf9b7bae1c8cca08316c41382
ChauAbdul786 <ChauAbdul786@gmail.com>
2023-08-09 14:43:23 -0700

    more bug fixes

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -5,14 +5,14 @@ import {Link, useNavigate} from 'react-router-dom'
 import CustomNavbar from "./CustomNavbar/CustomNavbar.js";
 
 function Signup() {
-    const [name, setName] = useState()
+    const [userName, setName] = useState()
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/register', {name, email, password})
+        axios.post('http://localhost:3001/register', {userName, email, password})
         .then(res => {
             navigate('/login')
         }).catch(err => console.log(err))
--- mongo/schema.js
+++ mongo/schema.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 const Schema = mongoose.Schema;
 
