0ade18fe6fde1d99ab7878ab5b0450e63c560e6a
ShaikeMukul <smuku004>
2023-08-14 15:48:01 -0700

    category and createdAt added

--- client/src/Components/AddListingModal/ProductLandingPage.js
+++ client/src/Components/AddListingModal/ProductLandingPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./ProductLandingPage.css";
+import { formatDistanceToNow } from "date-fns";
 import {
   EmailShareButton,
   EmailIcon,
@@ -53,6 +54,7 @@ const ProductLandingPage = ({ item }) => {
     console.log("Reply button clicked");
   };
 
+
   return (
     <div class="parent">
       <div className="div1">
@@ -90,7 +92,7 @@ const ProductLandingPage = ({ item }) => {
         <button onClick={handleReplyClick}>Reply</button>
       </div>
       <div className="div6">post id: {data._id}</div>
-      <div className="div7">posted: about an hour ago</div>
+      <div className="div7">posted: {formatDistanceToNow(new Date(data.createdAt), { addSuffix: true })}</div>
       <div className="div8">
         <iframe
           title="California Map"
